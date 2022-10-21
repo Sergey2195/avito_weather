236b11d6cc286c4e7e63b4ef8e15d158594f02d4
serser <Sergey21.95@gmail.com>
2022-10-21 12:09:22 +0300

    initial

---
+++ app/src/main/java/com/example/avitoweather/App.kt
@@ -0,0 +1,6 @@
+package com.example.avitoweather
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/com/example/avitoweather/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.avitoweather.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.avitoweather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
